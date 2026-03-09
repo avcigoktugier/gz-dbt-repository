821724db96380754ef77d8a609a48e8305b20709
recepgoktugavci@gmail.com <recepgoktugavci@gmail.com>
2026-03-09 05:02:34 +0000

    ship and Product renamed , data types changed

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -11,9 +11,9 @@ renamed as (
     select
         orders_id,
         shipping_fee,
-        shipping_fee_1,
+        -- shipping_fee_1 birebir aynı olduğu için çıkarıldı
         logcost,
-        ship_cost
+        TRUNC(CAST(ship_cost AS FLOAT64), 5) AS ship_cost
 
     from source
 
